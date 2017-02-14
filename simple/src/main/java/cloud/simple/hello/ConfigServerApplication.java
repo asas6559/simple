de25e55ed94a73adef1001c8a09c4b031475d373
asas6559 <302811456@qq.com>
2017-02-14 11:45:32 +0800

    test

---
+++ simple/src/main/java/cloud/simple/hello/ConfigServerApplication.java
@@ -0,0 +1,11 @@
+package cloud.simple.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ConfigServerApplication {
+
+
+}
---
+++ simple/src/main/java/cloud/simple/hello/SampleController.java
@@ -0,0 +1,29 @@
+package cloud.simple.hello;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class SampleController {
+	
+	
+    @ResponseBody
+    @RequestMapping(value = "/")
+    String home() {   
+        return "Hello World!";
+    }
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(SampleController.class, args);
+
+	}
+
+}
